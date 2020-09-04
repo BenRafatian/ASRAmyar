311d6a25f95c6dd789ba084d83fd130172120c87
wss <kurosh7899@gmail.com>
2020-09-04 13:42:49 +0430

    created login and logout templates and urls

--- FlowerShop/users/views.py
+++ FlowerShop/users/views.py
@@ -12,7 +12,7 @@ def register(request):
             username = form.cleaned_data.get('username')
             # TODO: add messages like below
             # messages.success(request, f'Account created for {username}.')
-            redirect('shop:products')
+            redirect('users:login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
