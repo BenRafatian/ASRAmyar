a53d406296a88ee1a078927588ba254b92e576c0
wss <kurosh7899@gmail.com>
2020-09-04 13:13:29 +0430

    added urls.py for users app and created 'register' url.

--- FlowerShop/FlowerShop/urls.py
+++ FlowerShop/FlowerShop/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls', namespace='shop')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
