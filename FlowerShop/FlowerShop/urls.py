27733db77afb88b2a28561deff9a4ced3aaf2741
wss <kurosh7899@gmail.com>
2020-09-06 20:42:28 +0430

    created cart's views, forms and urls

--- FlowerShop/FlowerShop/urls.py
+++ FlowerShop/FlowerShop/urls.py
@@ -5,8 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls', namespace='shop')),
     path('users/', include('users.urls', namespace='users')),
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('', include('shop.urls', namespace='shop')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
